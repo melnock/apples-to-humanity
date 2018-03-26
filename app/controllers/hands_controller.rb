54dc4245040a132ea1bad0837948b1aa8bc305f5
smarks8 <staceyamarks@gmail.com>
2018-03-26 14:58:31 -0400

    controller edits

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,2 +1,18 @@
 class HandsController < ApplicationController
+
+  def new
+    @hand = Hand.new
+  end
+
+  def create
+    @hand = Hand.new(hand_params)
+    @hand.save
+  end
+
+  private
+
+  def hand_params
+    params.require(:hand).permit(:deal_id, :card_id)
+  end
+
 end
